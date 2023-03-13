584ccc4296c6f14ae409d14810212ac6fbe60ab6
Thea Urne <theaurn@stud.ntnu.no>
2023-03-13 11:08:17 +0100

    Signed commit

---
+++ functions.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+void function1(int a, int b){
+    int c;
+
+    c = a + b;
+
+    cout << "If you add " << a << " and " << b << ", you get " << c << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <string>
+#include "functions.cpp"
 using namespace std;
 
 
 int main() {
     cout << "Hallo ja dette er en test";
 
+    function1(1, 2);
+
     return 0;
 }
-
-
