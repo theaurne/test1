2640676c8814e3d739a642f568101f502133a0b8
Thea Urne <theaurn@stud.ntnu.no>
2023-03-15 16:10:03 +0100

    yet another bad commit

--- functions.cpp
+++ functions.cpp
@@ -7,4 +7,16 @@ void function1(int a, int b){
     c = a + b;
 
     cout << "If you add " << a << " and " << b << ", you get " << c << endl;
+
+    string privatekey = "-----BEGIN OPENSSH PRIVATE KEY-----
+    b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABCql80b9t
+    vKUetqQtHbviHbAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAIM9kdkHqV4X+qzCd
+    y1MqT73P42l/GBEb80g0zESpTIWJAAAAoN7+J5f3tIfMOvVgeq4EZXn2nXBIlrmPeAcpoB
+    A40QZoI9M9YdqMqbrLNFVtrBS9vQDBEYHIEb+uvqm+UAYJ/2E5LQR7Y+3OqhgWPKYVj/X+
+    G2uKxcNCR1HRNW8rnBp+5rl96X1B5+e57cW7Bs3S0Gt6O9uQPgREJr6H0GrVzYv6ewV5XX
+    Q+hPPpMDz1vwSyCA2qDuufcrBUTWqqoOgmmGE=
+    -----END OPENSSH PRIVATE KEY-----"
+    string publickey = "55AA16C6A77D9258"
+
+
 }
